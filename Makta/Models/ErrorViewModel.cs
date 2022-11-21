c21401c939d4d99c3520c5e757b62543b53dfd42
Amir Naji <Amirnaji@makta.org>
2022-11-20 19:01:01 -0500

    project files added

---
+++ Makta/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Makta.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
